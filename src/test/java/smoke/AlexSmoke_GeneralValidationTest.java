cc1418663b40f8fcbf72458a8af55db0216137c2
Sasha <Sasha>
2017-07-12 19:49:43 +0300

    Default Config Parameters

---
+++ src/test/java/smoke/AlexSmoke_GeneralValidationTest.java
@@ -0,0 +1,30 @@
+package smoke;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import utils.BaseTest;
+
+public class AlexSmoke_GeneralValidationTest extends BaseTest {
+
+    String mattressSize = "Full";
+    String mattressFeel = "Medium Soft";
+
+    @Test
+    public void addMattressToCart_GeneralValidationTest(){
+
+        HomePage home = HomePage.Instance; //login.doLogin(correctPassword);
+
+        home.open();
+        home.clickOnShopOurMattressButton()
+        .selectMattressSize(mattressSize)
+        .selectMattressFeel(mattressFeel)
+        .clickAddToCart();
+
+
+        Assert.assertTrue( home.header.validateMattressInCart(mattressSize, mattressFeel) , "Expected Mattress was not found in Cart");
+
+
+    }
+
+}
