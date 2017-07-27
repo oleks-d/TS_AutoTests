1769d34b5b8c6b38087e6702ed3155d96595e795
Taras <sahaidachnyi@gmail.com>
2017-07-27 11:02:05 -0500

    comparing count of slip trackers (monitors) on cart icon and count of items in the cart - done

---
+++ src/test/java/smoke/Smoke_CountOfSleepTrackersInCart_Test.java
@@ -0,0 +1,52 @@
+package smoke;
+
+import annotations.TestName;
+import entities.ItemEntity;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.MonitorPage;
+import utils.BaseTest;
+import utils.EntitiesFactory;
+import utils.FileIO;
+
+/**
+ * @author Taras
+ * @since 7/27/2017.
+ */
+public class Smoke_CountOfSleepTrackersInCart_Test extends BaseTest {
+
+    @Test
+    @TestName(name = "Check count of sleep trackers in cart and cart flag in header")
+    public void countOfSleepTrackersInCart_Test() throws Exception {
+
+        int countOfGoodsFromCartIcon;
+        int countOfGoodsInCart;
+
+        //init test entities
+        ItemEntity defaultMonitor = EntitiesFactory.getItem(FileIO.getDataFile("Default_Monitor.json"));
+        ItemEntity updatedMonitor = EntitiesFactory.getItem(FileIO.getDataFile("Updated_Monitor.json"));
+
+        //Pages initializing
+        HomePage homePage = HomePage.Instance;
+        homePage.open();
+        MonitorPage monitorPage = homePage.clickOnShopOurMonitorButton();
+
+        //filling the cart with different types and counts of monitor
+        monitorPage.selectMonitorType(defaultMonitor.getType()).clickAddToCart();
+        monitorPage.selectMonitorType(defaultMonitor.getType()).clickAddToCart();
+        monitorPage.selectMonitorType(defaultMonitor.getType()).clickAddToCart();
+
+        monitorPage.selectMonitorType(updatedMonitor.getType()).clickAddToCart();
+        monitorPage.selectMonitorType(updatedMonitor.getType()).clickAddToCart();
+
+        //getting count of selected goods from cart icon
+        countOfGoodsFromCartIcon = monitorPage.header.getCountOfGoodsFromCartIcon();
+        //getting count of selected goods from cart
+        countOfGoodsInCart = monitorPage.header.getCountOfGoodsInCart();
+
+        //checking if monitors were added and counted
+        Assert.assertTrue(countOfGoodsInCart == countOfGoodsFromCartIcon, "Count of added to cart items equal to count from cart icon");
+    }
+
+}
