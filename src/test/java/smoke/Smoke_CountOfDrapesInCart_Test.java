4d650f1e3926b10a20d6ad2588bb59571185aba7
Taras <sahaidachnyi@gmail.com>
2017-07-28 06:26:03 -0500

    comparing count of Drapes on cart icon and count of items in the cart - done

---
+++ src/test/java/smoke/Smoke_CountOfDrapesInCart_Test.java
@@ -0,0 +1,60 @@
+package smoke;
+
+import annotations.TestName;
+import entities.ItemEntity;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DrapesPage;
+import pages.HomePage;
+import pages.ShopPage;
+import utils.BaseTest;
+import utils.EntitiesFactory;
+import utils.FileIO;
+
+/**
+ * @author Taras
+ * @since 7/27/2017.
+ */
+public class Smoke_CountOfDrapesInCart_Test extends BaseTest {
+
+    @Test
+    @TestName(name = "Check count of drapes in cart and cart flag in header")
+    public void countOfSheetsInCart_Test() throws Exception {
+
+        int countOfGoodsFromCartIcon;
+        int countOfGoodsInCart;
+
+        //creating system entities
+        ItemEntity defaultSheet = EntitiesFactory.getItem(FileIO.getDataFile("Default_Drapes.json"));
+        ItemEntity updatedSheet = EntitiesFactory.getItem(FileIO.getDataFile("Updated_Drapes.json"));
+
+        //Pages initializing
+        HomePage homePage = HomePage.Instance;
+        homePage.open();
+        ShopPage shopPage = homePage.header.clickShopMenuItem();
+        DrapesPage drapesPage= shopPage.clickOnShopOurDrapesButton();
+
+        //filling the cart with different types and count of drapes
+        drapesPage.selectDrapesColor(defaultSheet.getType()).clickAddToCart();
+        drapesPage.selectDrapesColor(defaultSheet.getType()).clickAddToCart();
+
+        //checking if items were added and counted normally
+        countOfGoodsFromCartIcon = drapesPage.header.getCountOfGoodsFromCartIcon();
+        countOfGoodsInCart = drapesPage.header.getCountOfGoodsInCart();
+        Assert.assertTrue(countOfGoodsInCart == countOfGoodsFromCartIcon, "Count of added to cart items equal to count from cart icon");
+
+
+        drapesPage.selectDrapesColor(updatedSheet.getType()).clickAddToCart();
+        drapesPage.selectDrapesColor(updatedSheet.getType()).clickAddToCart();
+        drapesPage.selectDrapesColor(updatedSheet.getType()).clickAddToCart();
+
+        //getting count of selected goods from cart icon
+        countOfGoodsFromCartIcon = drapesPage.header.getCountOfGoodsFromCartIcon();
+        //getting count of selected goods from cart
+        countOfGoodsInCart = drapesPage.header.getCountOfGoodsInCart();
+
+        //checking if drapes were added and counted
+        Assert.assertTrue(countOfGoodsInCart == countOfGoodsFromCartIcon, "Count of added to cart items equal to count from cart icon");
+    }
+
+}
