2882c5d1a6b068ab4a9c5b5fe1e9651d619683bf
dav1edave <fT2@^JjZ9>
2017-08-09 17:29:42 +0300

    Filter verification test is edited

--- src/main/java/pages/ReviewsRating.java
+++ src/main/java/pages/ReviewsRating.java
@@ -18,6 +18,7 @@ public class ReviewsRating extends BasePage {
 
     public PageHeader header = PageHeader.Instance;
 
+
     /**
      * UI Mappings
      */
@@ -53,12 +54,38 @@ public class ReviewsRating extends BasePage {
 
     // Applied filter buttons
 
-    By filterFiveStarsButton = By.xpath(".//span[contains(.,'5 Stars')]");
-    By filterFourStarsButton = By.xpath(".//span[contains(.,'4 Stars')]");
-    By filterThreeStarsButton = By.xpath(".//span[contains(.,'3 Stars')]");
-    By filterTwoStarsButton = By.xpath(".//span[contains(.,'2 Stars')]");
-    By filterOneStarButton = By.xpath(".//span[contains(.,'1 Star')]");
+    By xFiveStarsButton = By.xpath(".//span[contains(.,'5 Stars')]");
+    By xFourStarsButton = By.xpath(".//span[contains(.,'4 Stars')]");
+    By xThreeStarsButton = By.xpath(".//span[contains(.,'3 Stars')]");
+    By xTwoStarsButton = By.xpath(".//span[contains(.,'2 Stars')]");
+    By xOneStarButton = By.xpath(".//span[contains(.,'1 Star')]");
 
+
+    // Non clicked Histogram Filters
+
+    By fiveStarsFilterHistogram = By.xpath(".//*[@class='pr-histogram-5Stars  pr-histogram-stars']");
+    By fourStarsFilterHistogram = By.xpath(".//*[@class='pr-histogram-4Stars  pr-histogram-stars']");
+    By threeStarsFilterHistogram = By.xpath(".//*[@class='pr-histogram-3Stars  pr-histogram-stars']");
+    By twoStarsFilterHistogram = By.xpath(".//*[@class='pr-histogram-2Stars  pr-histogram-stars']");
+    By oneStarFilterHistogram = By.xpath(".//*[@class='pr-histogram-1Stars  pr-histogram-stars']");
+
+
+    // Clicked Histogram Filters
+
+    By fiveStarsFilterHistogramClicked = By.xpath(".//*[@class='pr-histogram-5Stars  pr-histogram-stars pr-hist-filter-5']");
+    By fourStarsFilterHistogramClicked = By.xpath(".//*[@class='pr-histogram-4Stars  pr-histogram-stars pr-hist-filter-4']");
+    By threeStarsFilterHistogramClicked = By.xpath(".//*[@class='pr-histogram-3Stars  pr-histogram-stars pr-hist-filter-3']");
+    By twoStarsFilterHistogramClicked = By.xpath(".//*[@class='pr-histogram-2Stars  pr-histogram-stars pr-hist-filter-2']");
+    By oneStarFilterHistogramClicked = By.xpath(".//*[@class='pr-histogram-1Stars  pr-histogram-stars pr-hist-filter-1']");
+
+
+    // Reting reviews locators
+
+    By rated5outOf5stars = By.xpath(".//span[contains(.,'Rated 5 out of 5 stars')]");
+    By rated4outOf5stars = By.xpath(".//span[contains(.,'Rated 4 out of 5 stars')]");
+    By rated3outOf5stars = By.xpath(".//span[contains(.,'Rated 3 out of 5 stars')]");
+    By rated2outOf5stars = By.xpath(".//span[contains(.,'Rated 2 out of 5 stars')]");
+    By rated1outOf5stars = By.xpath(".//span[contains(.,'Rated 1 out of 5 stars')]");
 
 
     /**
@@ -116,71 +143,69 @@ public class ReviewsRating extends BasePage {
     }
 
 
+    // Filter bar methods (Unnecessary)
 
-    // Filter bar methods
+//    public boolean filterBarsAvailabilityVerification(){
+//        reporter.info("Checking filter bars for availability");
+//        return isElementPresentAndDisplay(filterBar5)
+//                && isElementPresent(filterBar4)
+//                && isElementPresent(filterBar3)
+//                && isElementPresent(filterBar2)
+//                && isElementPresent(filterBar1);
+//    }
 
 
-    public boolean clickOnFiveStarsBar(){
-        isElementPresent(filterBar5);
+
+    // Filter Bar methods
+
+    public void clickOnFiveStarsBar(){ //ver. 1
         if(findElement(filterBar5).getText().contains("0")) {
-            reporter.info("No reviews found. Can't click on filter bar");
+            reporter.info("0 reviews found. Can't click on 5 stars filter bar");
         }
         else{
             findElement(filterBar5).click();
             reporter.info("Click on 5 stars filter bar");
         }
-        return true;
     }
 
-
-    public boolean clickOnFourStarsBar(){
-        isElementPresent(filterBar4);
+    public void clickOnFourStarsBar(){ //ver. 1
         if(findElement(filterBar4).getText().contains("0")) {
-            reporter.info("No reviews found. Can't click on filter bar");
+            reporter.info("0 reviews found. Can't click on 4 stars filter bar");
         }
         else{
             findElement(filterBar4).click();
             reporter.info("Click on 4 stars filter bar");
         }
-        return true;
     }
 
-
-    public boolean clickOnThreeStarsBar(){
-        isElementPresent(filterBar3);
+    public void clickOnThreeStarsBar(){ //ver. 1
         if(findElement(filterBar3).getText().contains("0")) {
-            reporter.info("No reviews found. Can't click on filter bar");
+            reporter.info("0 reviews found. Can't click on 3 stars filter bar");
         }
         else{
             findElement(filterBar3).click();
             reporter.info("Click on 3 stars filter bar");
         }
-        return true;
     }
 
-    public boolean clickOnTwoStarsBar(){
-        isElementPresent(filterBar2);
+    public void clickOnTwoStarsBar(){ //ver. 1
         if(findElement(filterBar2).getText().contains("0")) {
-            reporter.info("No reviews found. Can't click on filter bar");
+            reporter.info("0 reviews found. Can't click on 2 stars filter bar");
         }
         else{
             findElement(filterBar2).click();
             reporter.info("Click on 2 stars filter bar");
         }
-        return true;
     }
 
-
-    public boolean clickOnOneStarBar(){
-        isElementPresent(filterBar1);
+    public void clickOnOneStarsBar(){ //ver. 1
         if(findElement(filterBar1).getText().contains("0")) {
-            reporter.info("No reviews found. Can't click on filter bar");
+            reporter.info("0 reviews found. Can't click on 1 star filter bar");
         }
         else{
             findElement(filterBar1).click();
             reporter.info("Click on 1 star filter bar");
         }
-        return true;
     }
 
 
@@ -188,66 +213,187 @@ public class ReviewsRating extends BasePage {
     //  Filter removal methods
 
 
-    public boolean clickOnFilterFiveStarsButton(){
-
-        if (isElementPresentAndDisplay(filterFiveStarsButton)) {
-            reporter.info("Five Stars Button is found");
-            findElement(filterFiveStarsButton).click();
-            reporter.info("Click on 5 stars filter button");
+    public void removeFiveStarsFilter(){ //ver. 2
+        if(isElementPresentAndDisplay(xFiveStarsButton)){
+            findElement(xFiveStarsButton).click();
+            reporter.info("Click on 5 stars filter button and removing filter");
         }else{
-            reporter.info("Five Stars Filter Button is not found");
+            reporter.info("5 Stars filter removal button was not found");
         }
-        return false;
     }
 
-    public boolean clickOnFilterFourStarsButton(){
-
-        if (isElementPresentAndDisplay(filterFourStarsButton)) {
-            reporter.info("Four Stars Button is found");
-            findElement(filterFourStarsButton).click();
-            reporter.info("Click on 4 stars filter button");
+    public void removeFourStarsFilter(){ //ver. 2
+        if(isElementPresentAndDisplay(xFourStarsButton)){
+            findElement(xFourStarsButton).click();
+            reporter.info("Click on 4 stars filter button and removing filter");
         }else{
-            reporter.info("Four Stars Filter Button is not found");
+            reporter.info("4 Stars filter removal button was not found");
         }
-        return false;
     }
 
-    public boolean clickOnFilterThreeStarsButton(){
-
-        if (isElementPresentAndDisplay(filterThreeStarsButton)) {
-            reporter.info("Three Stars Button is found");
-            findElement(filterThreeStarsButton).click();
-            reporter.info("Click on 3 stars filter button");
+    public void removeThreeStarsFilter(){ //ver. 2
+        if(isElementPresentAndDisplay(xThreeStarsButton)){
+            findElement(xThreeStarsButton).click();
+            reporter.info("Click on 3 stars filter button and removing filter");
         }else{
-            reporter.info("Three Stars Filter Button is not found");
+            reporter.info("3 Stars filter removal button was not found");
         }
-        return false;
     }
 
-
-    public boolean clickOnFilterTwoStarsButton(){
-
-        if (isElementPresentAndDisplay(filterTwoStarsButton)) {
-            reporter.info("Two Stars Button is found");
-            findElement(filterTwoStarsButton).click();
+    public void removeTwoStarsFilter(){ //ver. 2
+        if(isElementPresentAndDisplay(xTwoStarsButton)){
+            findElement(xTwoStarsButton).click();
             reporter.info("Click on 2 stars filter button");
         }else{
-            reporter.info("Two Stars Filter Button is not found");
+            reporter.info("2 Stars filter removal button was not found");
         }
-        return false;
     }
 
-    public boolean clickOnFilterOneStarButton(){
-
-        if (isElementPresentAndDisplay(filterOneStarButton)) {
-            reporter.info("One Stars Button is found");
-            findElement(filterOneStarButton).click();
-            reporter.info("Click on 1 star filter button");
+    public void removeOneStarFilter(){ //ver. 2
+        if(isElementPresentAndDisplay(xOneStarButton)){
+            findElement(xOneStarButton).click();
+            reporter.info("Click on 1 stars filter button");
         }else{
-            reporter.info("One Stars Filter Button is not found");
+            reporter.info("1 Star filter removal button was not found");
         }
-        return false;
     }
+
+
+
+    //Filter successful removal verification
+
+    public boolean fiveStarsFilterVerification(){
+        if(isElementPresentAndDisplay(xFiveStarsButton)){
+            findElement(xFiveStarsButton).getText().contains("5 Stars");
+            reporter.info("5 Stars Removal button is still present");
+            return false;
+        }else {
+            reporter.info("5 Stars Removal button is not present");
+        }
+        return true;
+    }
+
+    public boolean fourStarsFilterVerification(){
+        if(isElementPresentAndDisplay(xFourStarsButton)){
+            findElement(xFourStarsButton).getText().contains("4 Stars");
+            reporter.info("4 Stars Removal button is still present");
+            return false;
+        }else {
+            reporter.info("4 Stars Removal button is not present");
+        }
+        return true;
+    }
+
+    public boolean threeStarsFilterVerification(){
+        if(isElementPresentAndDisplay(xFiveStarsButton)){
+            findElement(xThreeStarsButton).getText().contains("3 Stars");
+            reporter.info("3 Stars Removal button is still present");
+            return false;
+        }else {
+            reporter.info("3 Stars Removal button is not present");
+        }
+        return true;
+    }
+
+    public boolean twoStarsFilterVerification(){
+        if(isElementPresentAndDisplay(xTwoStarsButton)){
+            findElement(xTwoStarsButton).getText().contains("2 Stars");
+            reporter.info("3 Stars Removal button is still present");
+            return false;
+        }else {
+            reporter.info("3 Stars Removal button is not present");
+        }
+        return true;
+    }
+
+    public boolean oneStarFilterVerification(){
+        if(isElementPresentAndDisplay(xTwoStarsButton)){
+            findElement(xTwoStarsButton).getText().contains("1 Star");
+            reporter.info("1 Star Removal button is still present");
+            return false;
+        }else {
+            reporter.info("1 Star Removal button is not present");
+        }
+        return true;
+    }
+
+
+//    public boolean twoStarsFilterVerification(){ // OLD
+//        isElementPresentAndDisplay(twoStarsFilterHistogram);
+//        reporter.info("2 Stars Bar is found and active. Filter removed successfully");
+//        return true;
+//    }
+
+//    public boolean clickOnFilterFiveStarsButton(){ //Ver 1 OLD
+//
+//        if (isElementPresentAndDisplay(filterFiveStarsButton)) {
+//            reporter.info("Five Stars Button is found");
+//            findElement(filterFiveStarsButton).click();
+//            reporter.info("Click on 5 stars filter button");
+//        }else{
+//            reporter.info("Five Stars Filter Button is not found");
+//        }
+//        return false;
+//    }
+
+
+    // Reviews verification
+
+    public boolean isFiveStarsReviewPresent(){ //TODO playing with returns in case no active button or reviews
+        if(findElement(filterBar5).getText().contains("0")){
+            reporter.info("5 Stars Reviews are not found. 0 reviews");
+        } else{
+            isElementPresentAndDisplay(rated5outOf5stars);
+            reporter.info("5 Stars Reviews are found");
+        }
+        return true;
+    }
+
+    public boolean isFourStarsReviewPresent(){ //TODO playing with returns in case no active button or reviews
+        if(findElement(filterBar4).getText().contains("0")){
+            reporter.info("4 Stars Reviews are not found. 0 reviews");
+        } else{
+            isElementPresentAndDisplay(rated4outOf5stars);
+            reporter.info("4 Stars Reviews are found");
+        }
+        return true;
+    }
+
+    public boolean isThreeStarsReviewPresent(){ //TODO playing with returns in case no active button or reviews
+        if(findElement(filterBar3).getText().contains("0")){
+            reporter.info("3 Stars Reviews are not found. 0 reviews");
+        } else{
+            isElementPresentAndDisplay(rated3outOf5stars);
+            reporter.info("3 Stars Reviews are found");
+        }
+        return true;
+    }
+
+    public boolean isTwoStarsReviewPresent(){ //TODO playing with returns in case no active button or reviews
+        if(findElement(filterBar2).getText().contains("0")){
+            reporter.info("2 Stars Reviews are not found. 0 reviews");
+        } else{
+            isElementPresentAndDisplay(rated2outOf5stars);
+            reporter.info("2 Stars Reviews are found");
+        }
+        return true;
+    }
+
+    public boolean isOneStarReviewPresent(){ //TODO playing with returns in case no active button or reviews
+        if(findElement(filterBar1).getText().contains("0")){
+            reporter.info("1 Star Reviews are not found. 0 reviews");
+        } else{
+            isElementPresentAndDisplay(rated1outOf5stars);
+            reporter.info("1 Star Reviews are found");
+        }
+        return true;
+    }
+
+//    public boolean isOneStarReviewPresent(){ //OLD
+//        isElementPresentAndDisplay(rated1outOf5stars);
+//        reporter.info("One Star Reviews are found");
+//        return true;
+//    }
 
 }
 
